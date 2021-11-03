748719749a8ca9315b78b8e43373d941d04bc365
leandro-ikehara <leandro.ikehara@gmail.com>
2021-11-03 11:42:19 -0300

    terceiro commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cards from "../components/cards/Cards";
+
+function Home (props) {
+
+    return (
+        <>
+        <Cards />
+        </>
+    )
+}
+
+export default Home
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/Home';
 
 export const Routes = () => {
-    return {
+    return (
         <Switch>
-        <Route path = "/" component={Dashboard} exact />
-        // <Route path = "/cards" component={Cards} />
-        // <Route path = "/pricing" component={Pricing} />
-        // <Route path = "/blocks" component={Blocks} />
-        // <Route path = "/forms" component={Forms} />
+        <>
+        <Route path="/" component={Home} />
+        {/* <Route path = "/cards" component={Cards} />
+        <Route path="/pricing" component={Pricing} />
+        <Route path = "/blocks" component={Blocks} />
+        <Route path = "/forms" component={Forms} /> */}
+        </>
         </Switch>
-    }
+    )
 }
